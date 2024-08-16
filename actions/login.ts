2e07e68ebf57d40eebbdc498d6d792d46a8571d7
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-08-16 12:27:09 +0500

    handle success and error case

--- actions/login.ts
+++ actions/login.ts
@@ -6,9 +6,9 @@ import * as z from "zod";
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validated = LoginSchema.safeParse(values);
 
-    // if (!validated.success) {
-    return { error: "Invalid fields" };
-    // }
+    if (!validated.success) {
+        return { error: "Invalid fields" };
+    }
 
     return { success: "Email Sent" };
 };
